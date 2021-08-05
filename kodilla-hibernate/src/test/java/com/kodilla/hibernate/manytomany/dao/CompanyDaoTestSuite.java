3ed2949d789bd3139cab62516ffdc8cd5c52bb1b
Paulina Leszczynska <paulinarudz@wp.pl>
2021-08-05 12:20:41 +0200

    17.4 NamedQuery in Company and Employee with tests

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
@@ -13,6 +16,8 @@ public class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -50,12 +55,57 @@ public class CompanyDaoTestSuite {
         assertNotEquals(0, greyMatterId);
 
         //CleanUp
-        //try {
-        //    companyDao.deleteById(softwareMachineId);
-        //    companyDao.deleteById(dataMaestersId);
-        //    companyDao.deleteById(greyMatterId);
-        //} catch (Exception e) {
-        //    //do nothing
-        //}
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+    @Test
+    void testRetrieveCompanyWithNameStartingWith() {
+        //Given
+        Company company1 = new Company("First Company");
+        Company company2 = new Company("Second Company");
+        Company company3 = new Company("Third Company");
+        //When
+        companyDao.save(company2);
+        int company2Id = company2.getId();
+        companyDao.save(company1);
+        int company1Id = company1.getId();
+        companyDao.save(company3);
+        int company3Id = company3.getId();
+
+        List<Company> companyListWithNameStarting = companyDao.retrieveCompanyWithNameStartingWith("Fir");
+
+        //Then
+        assertEquals(1, companyListWithNameStarting.size());
+        //CleanUp
+        companyDao.deleteById(company1Id);
+        companyDao.deleteById(company2Id);
+        companyDao.deleteById(company3Id);
+    }
+    @Test
+    void testRetrieveEmployeeWithLastName() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+        //When
+        employeeDao.save(johnSmith);
+        employeeDao.save(stephanieClarckson);
+        employeeDao.save(lindaKovalsky);
+
+        List<Employee> employeeListWithGivenLastName = employeeDao.retrieveEmployeeWithLastName("Smith");
+
+        //Then
+        assertEquals(1, employeeListWithGivenLastName.size());
+
+        //CleanUp
+        employeeDao.delete(johnSmith);
+        employeeDao.delete(stephanieClarckson);
+        employeeDao.delete(lindaKovalsky);
+
     }
 }
