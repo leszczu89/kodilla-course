de888e9aeba7857576e6cc0eed5099bf2b386059
Paulina Leszczynska <paulinarudz@wp.pl>
2021-09-17 14:23:18 +0200

    Added ForumTopic with Observable int.

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -5,6 +5,7 @@ import com.kodilla.patterns2.facade.api.OrderDto;
 import com.kodilla.patterns2.facade.api.OrderFacade;
 import com.kodilla.patterns2.facade.api.OrderProcessingException;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
