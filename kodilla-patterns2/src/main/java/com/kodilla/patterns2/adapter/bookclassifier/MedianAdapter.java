ae9b6e720c7f5a992076a16e87d6f26e65345a1a
Paulina Leszczynska <paulinarudz@wp.pl>
2021-10-28 09:09:31 +0200

    Added storedProcTestSuite

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -3,7 +3,6 @@ package com.kodilla.patterns2.adapter.bookclassifier;
 import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
-import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookStatistics;
 
 import java.util.HashMap;
 import java.util.Map;
